96ebb8b506ad5688b583c36ad8b4d47776a2e436
kevinLacoste <k.b.h.lacoste@gmail.com>
2018-12-16 12:41:34 +0100

    Ajout des NearestNeighbors et traitement des images par vecteur ligne

--- MDClassifier.py
+++ MDClassifier.py
@@ -5,104 +5,190 @@ import time
 import test
 import preprocessing as pr
 from sklearn.decomposition import PCA
-import png
+import sklearn.svm as svm
+import sklearn.neighbors as neighbors
 import SKClassifier as sk
 
-print(" ** MDClassifier.py : starting program")
-print(" ** MDClassifier.py : loading data files")
-
-train_data = loadmat("../Data/train_32x32.mat")
-# ~ test_data = loadmat("../Data/test_32x32.mat")
-test_data = sk.vectors_reduction(loadmat("../Data/test_32x32.mat"), 2)
-# ~ pca_data = train_data.copy()
-pca_data = sk.vectors_reduction(train_data, 2)
-
-# ~ for i in range(1) :
-    # ~ picture = pca_data['X'][:,:,:,i]
-    # ~ nsamples, x, y = pca_data['X'][:, :, :, i].shape
-    # ~ X = picture.reshape(nsamples, x*y)
-    # ~ for i in range(x) :
-        # ~ print(X[i])
-    # ~ # PCA 
-    # ~ pca = PCA(n_components=32)
-    # ~ pca.fit(X)
-    # ~ X = pca.components_
-    # ~ plt.imshow(X)
-    # ~ plt.show()
+class DMIN() :
     
-
-print(" ** MDClassifier.py : applying pre-processing")
-
-def calculate_barycenters(data):
-
-    print(" ** MDClassifier.py : splitting vectors by classes")
-    allVectorsSt = []
+    def __init__(self) :
+        self.barycenters = {}
     
-    # Records numbers of dimensions of data['X']
-    data_shape_len = len(np.shape(data['X']))
-    
-    for i in range(10):
-        allVectorsSt.append([])
-    
-    for i in range(len(data['y'])):
-        if data_shape_len == 4 :
-            allVectorsSt[data['y'][i][0]-1].append(data['X'][:,:,:,i])
-        elif data_shape_len == 3 :
-            allVectorsSt[data['y'][i][0]-1].append(data['X'][:,:,i])
-        else : 
-            return False
-    
-    allVectors = np.array(allVectorsSt)
-    
-    print("allVectors : ", np.shape(allVectors[0]))
-    print(" ** MDClassifier.py : splitting done")
-
-    barycenters = {}
-    print(" ** MDClassifier.py : starting calculation of barycenters")
-    for i in range(10):
-        if (len(allVectors[i]) != 0):
-            barycenters[i+1] = np.mean(allVectors[i], axis = 0)
-    print(" ** MDClassfier.py : calculation done")
-    print("barycenters : ", np.shape(barycenters[1]))
-    return barycenters
-
-
-def find_classes(picture, barycenters) :
-    idClass = 1
-    mtxNorm = np.linalg.norm(picture - barycenters[idClass])
-    for i in range(2,11):
-        if(np.linalg.norm(picture - barycenters[i]) < mtxNorm):
-            mtxNorm = np.linalg.norm(picture - barycenters[i])
-            idClass = i
-    return idClass
-
-def calculate_success_rate(test, train):
-    data_shape_len = len(np.shape(train['X']))
-    success = 0
-    bc = calculate_barycenters(train)
-    for i in range(len(test['y'])):
-        if data_shape_len == 4 :
-            idClass = find_classes(test['X'][:,:,:,i], bc)
-        elif data_shape_len == 3 :
-            idClass = find_classes(test['X'][:,:,i], bc)
-        else : 
+    # Train the model by calculating barycenters for each label
+    def train(self, train_data, train_labels) :
+        print(" ** MDClassifier.py : starting training of DMIN classifier")
+        print(" ** MDClassifier.py : splitting vectors by classes")
+        nb_train, nb_pixels = np.shape(train_data)
+        nb_labels = np.shape(train_labels)[0]
+        
+        # Verification 
+        if nb_train > nb_labels :
+        #Exception here
+            print("Error : training not possible (DMIN.train())")
             return False
             
-        if (idClass == test['y'][i]):
+        allVectorsSt = []
+        
+        for i in range(10):
+            allVectorsSt.append([])
+        
+        for i in range(nb_train):
+            allVectorsSt[train_labels[i]-1].append(train_data[i])
+        
+        allVectors = np.array(allVectorsSt)
+        print(" ** MDClassifier.py : splitting done")
+        
+        print(" ** MDClassifier.py : starting calculation of barycenters")
+        for i in range(10):
+            if (len(allVectors[i]) != 0):
+                self.barycenters[i+1] = np.mean(allVectors[i], axis = 0)
+        print(" ** MDClassifier.py : calculation done")
+            
+    # Predicts for the given picture the corresponding label. The picture 
+    # must use the pre-processing of the training data
+    def find_class(self, picture) :
+        idClass = 1
+        mtxNorm = np.linalg.norm(picture - barycenters[idClass])
+        for i in range(2,11):
+            if(np.linalg.norm(picture - barycenters[i]) < mtxNorm):
+                mtxNorm = np.linalg.norm(picture - barycenters[i])
+                idClass = i
+        return idClass
+
+    def find_class_array(self, test_data) :
+        predicted_labels = []
+        nb_test, nb_pixels = np.shape(test_data)
+        
+        for sel in range(nb_test) :
+            picture = test_data[sel]
+            actual_id_class = 1
+            mtxNorm = np.linalg.norm(picture - self.barycenters[actual_id_class])
+            for i in range(2,11):
+                if(np.linalg.norm(picture - self.barycenters[i]) < mtxNorm):
+                    mtxNorm = np.linalg.norm(picture - self.barycenters[i])
+                    actual_id_class = i
+            predicted_labels.append(actual_id_class)
+            
+        return predicted_labels
+    
+def calculate_success_rate(test_labels, predicted_labels) :
+    success = 0
+    for i in range(len(predicted_labels)):
+        if (predicted_labels[i] == test_labels[i]):
             success += 1
-    successRate = 100*success/len(test["y"])
+    successRate = 100*success/len(predicted_labels)
     return success, successRate
 
 if __name__ == "__main__":
     
+    print(" ** MDClassifier.py : starting program \n")
+    
+    # Loading .mat files (originals or pre-processed)
+    print(" ** MDClassifier.py : loading data files")
+    train_mat = loadmat("../Data/train_32x32.mat")
+    test_mat  = loadmat("../Data/test_32x32.mat")
+    
+    x, y, nsamples, nb_train = np.shape(train_mat['X'])
+    x, y, nsamples, nb_test = np.shape(test_mat['X'])
+    
+    # Setting training data into line vectors
+    train_data = np.ndarray(shape=(nb_train, x*y*nsamples), dtype=np.uint8)
+    train_labels = np.ndarray(shape=(nb_train), dtype=np.uint8)
+    for i in range(nb_train) :
+        train_data[i] = train_mat['X'][:, :, :, i].copy().flatten()
+        train_labels[i] = train_mat['y'][i][0]
+    
+    # Setting testing data into line vectors
+    test_data = np.ndarray(shape=(nb_test, x*y*nsamples), dtype=np.uint8)
+    test_labels = np.ndarray(shape=(nb_test), dtype=np.uint8)
+    for i in range(nb_test) :
+        test_data[i] = test_mat['X'][:, :, :, i].copy().flatten()
+        test_labels[i] = test_mat['y'][i][0]
+    
+    print()
+    
+    # Initialising clock for time duration (PCA + normal)
     computeStart = time.time()
     div = False
-    print(" ** MDClassifier.py : starting to compute learning vectors")
-    print(" ** MDClassifier.py : computing with default data")
-    [success, successRate] = calculate_success_rate(test_data, pca_data)
-    print("\n ** MDClassifier.py : success = " + str(success))
     
+    pca_train = sk.pca_filter(train_data, 20)
+    print(" ** MDClassifier.py : final pca_dat shape = ", np.shape(pca_train))
+    pca_test = sk.pca_filter(test_data, 20)
+    print(" ** MDKClassifier.py : final pca_dat shape = ", np.shape(pca_test))
+    
+    
+    # ********** DMIN classifier ******************* #
+    # ~ print(" ** MDClassifier.py : starting to compute learning vectors")
+    # ~ print(" ** MDClassifier.py : computing with default data")
+    # ~ dmin = DMIN() 
+    # ~ dmin.train(pca_train, train_labels)
+    # ~ predicted_labels = dmin.find_class_array(pca_test)
+    # ~ print()
+    
+    # ********** sklearn.SVM classifier ************ #
+    svc = svm.SVC()
+    print(" ** MDClassifier.py : starting SVC classifier")
+    print(" ** MDClassifier.py : training SVC")
+    svc.fit(pca_train, train_labels)
+    
+    div_mid = False
+    computeMid = time.time()
+    computeDurationMid = computeMid - computeStart
+    if computeDurationMid > 60 :
+        computeDurationMid = computeDurationMid / 60
+        div_mid = True
+    unit = " min" if div_mid else " s"
+    print(" ** MDClassifier.py : computing duration (SVC : training) = " + str(computeDurationMid) + unit)
+    
+    print(" ** MDClassifier.py : starting SVC predictions on test data")
+    predicted_labels = svc.predict(pca_test)
+    print(" ** MDClassifier.py : end of SVC predictions on test data")
+    
+    # ********** sklearn.neighbors classifier ****** #
+    # ~ print(" ** MDClassifier.py : starting Nearest Neighbors classifier")
+    # ~ nb_neighbors = 1 # Must be int >= 1
+    # ~ neighbors_kdtree = neighbors.NearestNeighbors(n_neighbors=nb_neighbors, algorithm='kd_tree')
+    # ~ print(" ** MDClassifier.py : training Nearest Neighbors model")
+    # ~ neighbors_kdtree.fit(pca_train, train_labels)
+    # ~ print(" ** MDClassifier.py : starting Nearest Neighbors predictions on test data")
+    # ~ distances, indices = neighbors_kdtree.kneighbors(pca_test)
+    # ~ predicted_labels = np.ndarray(shape=(len(pca_test)))
+    # ~ for i in range(len(pca_test)) :
+        # ~ mean_distances = {}
+        # ~ if nb_neighbors == 2 :
+            # ~ predicted_labels[i] = train_labels[indices[i]]
+        # ~ else : 
+            # ~ for nb in range(nb_neighbors) :
+                # ~ actual_label = train_labels[indices[i][nb]]
+                # ~ actual_distance = distances[i][nb]
+                
+                # ~ if actual_label not in mean_distances :
+                    # ~ mean_distances[actual_label] = []
+                # ~ mean_distances[actual_label].append(actual_distance)
+            
+            # ~ md_keys = list(mean_distances.keys())
+            # ~ min_label = md_keys[0]
+            # ~ min_distance = np.mean(mean_distances[min_label])
+            # ~ for label in md_keys :
+                # ~ if label != min_label :
+                    # ~ actual_distance = np.mean(mean_distances[label])
+                    # ~ if actual_distance < min_distance :
+                        # ~ min_label = label
+                        # ~ min_distance = actual_distance
+            # ~ predicted_labels[i] = min_label
+        
+    # ~ print(" ** MDClassifier.py : ending Nearest Neighbors predictions on test data")
+    
+    ##################################################
+    
+    # Determining success rate of model
+    print(" ** MDClassifier.py : calculating success rate on test data")
+    [success, successRate] = calculate_success_rate(test_labels, predicted_labels)
+    # ~ [success, successRate] = calculate_success_rate(pca_test_data, predicted_labels)
+    print(" ** MDClassifier.py : success = " + str(success))
     print(" ** MDClassifier.py : sucess rate = " + str(successRate) + "%")
+    print()
+    
     computeEnd = time.time()
     computeDuration = computeEnd - computeStart
     if computeDuration > 60 :
--- SKClassifier.py
+++ SKClassifier.py
@@ -2,48 +2,37 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.io import loadmat
 from sklearn import decomposition as dec
+import preprocessing as pr
+import sys
 
-def pca_filter(data, ncomp=2, ndata=0):
-
-    # Setting up pprocessing limit parameter
+def pca_filter(data, ncomp=0, ndata=0):
+    print(" ** SKClassifier.py : starting PCA decomposition (PCA_filter) ...")
+    
+    # Retrieving shape of data (if not correct : crash)
+    nb_data, nb_pixels = np.shape(data)
+    
+    # Setting up processing limit parameter
     ndata_int = int(ndata)
-    if ndata_int > len(data['y']) or ndata_int <= 0 :
-        ndata_int = len(data['y'])
-       
-    # Retreiving shape of pictures (should be same for all)
-    x, y, nsamples = data['X'][:, :, :, 0].shape
-    
-    # Creating a new dictionary for pca processing
-    dict_data = {}
-    labels = []
-    for label in data['y'][:ndata_int] :
-        labels.append(label)
-    dict_data['y'] = np.array(labels)
-    data_type = type(data['X'][0, 0, 0, 0])
-    updated_data = np.ndarray(shape=(x, y, nsamples, ndata_int), dtype=data_type)
-
+    if ndata_int > nb_data or ndata_int <= 0 :
+        ndata_int = nb_data
+        
+    # Limiting data input to limit parameter
+    nb_data = ndata_int
+    data = data[:nb_data]
+        
     # Creating PCA unit from Scikit-learn
-    pca = dec.PCA(ncomp)
+    if ncomp == 0 :
+        pca = dec.PCA()
+    else :
+        pca = dec.PCA(ncomp)
     
-    print("Starting PCA decomposition (PCA_filter) ...")
-    for i in range(ndata_int):
-        # Reshaping input picture to shape (nsamples, x*y) with x number
-        # of lines and y number of columns
-        picture = data['X'][:, :, :, i].copy()
-        tmp = picture.reshape(nsamples, x*y)
-        
-        # Applying pca transformation and inverse transformation on pic
-        pca_tmp = pca.fit_transform(tmp)
-        tmp = pca.inverse_transform(pca_tmp).astype(data_type)
-        
-        # Replacing old picture with new picture
-        updated_data[:, :, :, i] = tmp.reshape(x, y, nsamples)
-        
-    dict_data['X'] = updated_data
+    # Creating new ndarray for pca transformation
+    pca_data = pca.fit_transform(data)
     
-    print("Ending PCA decomposition (PCA_filter) ...")
+    print(" ** SKClassifier.py : final pca_dat shape = ", np.shape(pca_data))
     
-    return dict_data
+    print(" ** SKClassifier.py : ending PCA decomposition (PCA_filter) ...")
+    return pca_data
 
 
 def vectors_reduction(data, ncomp=2, ndata=0):
@@ -65,11 +54,10 @@ def vectors_reduction(data, ncomp=2, ndata=0):
     dict_data['y'] = np.array(labels)
     data_type = type(data['X'][0, 0, 0, 0])
     updated_data = np.ndarray(shape=(ncomp, x*y, ndata_int), dtype=data_type)
-    print("shape of updated_data : ", np.shape(updated_data))
 
     pca = dec.PCA(ncomp)
     
-    print("Starting PCA decomposition (vectors_reduction) ...")
+    print(" ** SKClassifier.py : starting PCA decomposition (vectors_reduction) ...")
     for i in range(ndata_int):
         picture = data['X'][:, :, :, i].copy()
         tmp = picture.reshape(x*y, nsamples)
@@ -78,11 +66,11 @@ def vectors_reduction(data, ncomp=2, ndata=0):
         
     dict_data['X'] = updated_data
     
-    print("Ending PCA decomposition (vectors_reduction) ...")
+    print(" ** SKClassifier.py : ending PCA decomposition (vectors_reduction) ...")
     
     return dict_data
     
-def image_reduction(data, ncomp=768, ndata=0):
+def image_reduction(data, ncomp=24, ndata=0):
 
     # Setting up pprocessing limit parameter
     ndata_int = int(ndata)
@@ -92,52 +80,27 @@ def image_reduction(data, ncomp=768, ndata=0):
     # Retreiving shape of pictures (should be same for all)
     x, y, nsamples = data['X'][:, :, :, 0].shape
     data_type = type(data['X'][0, 0, 0, 0])
-    
     dict_data = {}
     dict_data['y'] = data['y'][:ndata_int]
-    updated_data = np.ndarray(shape=(nsamples, ncomp, ndata_int), dtype=data_type)
-    print("shape of updated_data : ", np.shape(updated_data))
+    updated_data = np.ndarray(shape=(nsamples*x, ncomp, ndata_int), dtype=data_type)
 
-    pca = dec.PCA()
-    print(ncomp)
+    pca = dec.PCA(ncomp)
     
-    print("Starting PCA decomposition (vectors_reduction) ...")
+    print(" ** SKClassifier.py : starting PCA decomposition (image_vector_reduction) ...")
     for i in range(ndata_int):
-        tmp = data['X'][:, :, :, i].reshape(nsamples, x*y)
-        print("shape of tmp : ", np.shape(tmp))
+        picture = data['X'][:, :, :, i].copy()
+        tmp = picture.reshape(nsamples*x , y)
         pca_tmp = pca.fit_transform(tmp)
         updated_data[:, :, i] = pca_tmp
-        print(np.shape(pca_tmp))
         
     dict_data['X'] = updated_data
     
-    print("Ending PCA decomposition (vectors_reduction) ...")
+    print(" ** SKClassifier.py : ending PCA decomposition (image_vector_reduction) ...")
     
     return dict_data
 
 
 if __name__ == "__main__":
     train_data = loadmat("../Data/train_32x32.mat")
-    plt.imshow(train_data['X'][:,:,:,0])
-    plt.show()
-    pca = dec.PCA(2)
-    x, y, nsamples = train_data['X'][:, :, :, 0].shape
-    tmp = np.swapaxes(train_data['X'][:, :, :, 0], 0, 2).reshape(nsamples, x*y)
-    
-    # testpic
-    test_pic = np.zeros([32,32,3], dtype=np.uint8)
-    for i in range(32) :
-        for j in range(32) :
-            for p in range(3) :
-                test_pic[i,j,p] = tmp[p, i*32+ j]
-    
-    plt.imshow(test_pic)
-    plt.show()
-    
-    tmp_transformed = pca.fit_transform(tmp)
-    new_picture = np.swapaxes(pca.inverse_transform(tmp_transformed), 0, 1).reshape(x, y, nsamples)
-    new_picture = np.swapaxes(new_picture, 0, 1)
-    plt.imshow(new_picture)
-    plt.show()
-    # ~ vectors_reduction(1, train_data)
+    pca_filter(train_data, 0.98, 100)
     
